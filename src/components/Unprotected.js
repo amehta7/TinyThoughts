79d5eeaba02d4466b1e4b790e19663cd33f013c6
amehta7 <amimehta2811@gmail.com>
2022-08-04 18:07:32 -0700

    final changes

--- src/components/Unprotected.js
+++ src/components/Unprotected.js
@@ -4,13 +4,7 @@ import { Route, Redirect } from 'react-router-dom'
 const Unprotected = memo(({ user, children, ...rest }) => (
   <Route
     {...rest}
-    render={({ location }) =>
-      !user ? (
-        children
-      ) : (
-        <Redirect to={{ pathname: '/', state: { from: location } }} />
-      )
-    }
+    render={() => (!user ? children : <Redirect to={{ pathname: '/' }} />)}
   />
 ))
 
